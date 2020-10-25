5df646f6fba601a2379604598246b0985ddf1548
Xxone-ai <57640369+Xxone-ai@users.noreply.github.com>
2020-10-25 23:32:34 +0300

    Прототип класса Person и  move-триггеров

---
+++ Person.cpp
@@ -0,0 +1,26 @@
+#include "Person.h"
+
+void Person::moveright()
+{
+	right_pressed = true;
+}
+
+void Person::moveleft()
+{
+	left_pressed = true;
+}
+
+void Person::movedown()
+{
+	down_pressed = true;
+}
+
+void Person::moveupper()
+{
+	upper_pressed = true;
+}
+
+void Person::update()
+{
+	// необходимо написать функцию задания позиции.
+}
---
+++ Person.h
@@ -0,0 +1,17 @@
+#pragma once
+class Person
+{
+private:
+	bool left_pressed;
+	bool right_pressed;
+	bool down_pressed;
+	bool upper_pressed;
+	//Необходима доработка класса, данная часть кода - набросок.
+public:
+	void moveright();
+	void moveleft();
+	void movedown();
+	void moveupper();
+	void update();
+};
+
