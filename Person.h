c10c3433ecd9c4cf2fad6c7ec6b1b7965e7f3c6b
Sershhka <57639730+Sershhka@users.noreply.github.com>
2020-10-26 09:03:14 +0400

    fastFix

--- Person.h
+++ Person.h
@@ -1,13 +1,9 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+
 class Person
 {
 private:
-	bool left_pressed;
-	bool right_pressed;
-	bool down_pressed;
-	bool upper_pressed;
-
 	bool gravitationCheck; //в будующем планируется сделать массив со всеми остальными параметрами
 	float dx;
 	float dy;
@@ -19,6 +15,7 @@ private:
 public:
 	Person();
 	Person(std::string pathToFile);
+	// в каждый метод движения будет передаваться разрешённость этого движения
 	void moveRight();
 	void moveLeft();
 	void moveDown();
