af9ba87ecf09715d06b42432a0c9f44e57d8b2a4
Xxone-ai <57640369+Xxone-ai@users.noreply.github.com>
2020-11-01 11:27:22 +0300

    Fast fix

--- Tile.cpp
+++ Tile.cpp
@@ -3,20 +3,26 @@
 Tile::Tile()
 {
 	hardness = 0;
+	size_x = 0;
+	size_y = 0;
 }
 
-Tile::Tile(bool hardness, std::string path_to_texture)
+Tile::Tile(bool hardness, std::string path_to_texture, int x, int y)
 {
+	this->size_x = x;
+	this->size_y = y;
 	this->hardness = hardness;
 	sf::Texture texture;
 	texture.loadFromFile(path_to_texture);
 	this->sprite.setTexture(texture);
-
+	this->sprite.setTextureRect(sf::IntRect(0, 0, this->size_x, this->size_y));
 }
 
 Tile::Tile(const Tile& obj)
 {
-	hardness = obj.hardness;
+	this->size_x = obj.size_x;
+	this->size_y = obj.size_y;
+	this->hardness = obj.hardness;
 }
 
 sf::Sprite Tile::Get_Sprite()
--- Tile.h
+++ Tile.h
@@ -5,10 +5,11 @@ class Tile
 protected:
 	bool hardness;
 	sf::Sprite sprite;
-	//std::string TexturePath;
+	int size_x;
+	int size_y;
 public:
 	Tile();
-	Tile(bool hardness, std::string path_to_texture);
+	Tile(bool hardness, std::string path_to_texture, int x, int y);
 	Tile(const Tile& obj);
 	sf::Sprite Get_Sprite();
 };
