fb06022636b47e17ab59ae538373aa50b2c9a7dc
Xxone-ai <57640369+Xxone-ai@users.noreply.github.com>
2020-11-22 19:22:55 +0300

    Tile.h&Tile.cpp

--- GameMap.h
+++ GameMap.h
@@ -1,61 +1,17 @@
 #pragma once
 #include "SFML/Graphics.hpp"
+#include <iostream>
+#include "Tile.h"
+#include <map>
+
 class GameMap
 {
 private:
-	class Tile
-	{
-		public:
-		sf::Vector2f TileFilePos;
-		std::string PathToFile;
-		sf::Vector2u Size;
-		sf::Texture SprTexture;
-		sf::Sprite TileSprite;
-		Tile (sf::Vector2f pos_in_file, std::string PathToFile, sf::Vector2u size)
-		{
-			this->PathToFile = PathToFile;
-			this->TileFilePos = pos_in_file;
-			this->Size = size;
-			SprTexture.loadFromFile(PathToFile);
-			TileSprite.setTextureRect(sf::IntRect(TileFilePos.x, TileFilePos.y, Size.x, Size.y));
-		}
-		Tile()
-		{
-			this->TileFilePos = sf::Vector2f();
-			this->PathToFile = "0";
-		}
-		sf::Sprite& GetSprite()
-		{
-			return this->TileSprite;
-		}
-		Tile(const Tile& obj)
-		{
-			this->PathToFile = obj.PathToFile;
-			this->Size = obj.Size;
-			this->SprTexture = obj.SprTexture;
-			this->TileFilePos = obj.TileFilePos;
-			this->TileSprite = obj.TileSprite;
-		}
-		Tile& operator = (const Tile& obj)
-		{
-			this->PathToFile = obj.PathToFile;
-			this->Size = obj.Size;
-			this->SprTexture = obj.SprTexture;
-			this->TileFilePos = obj.TileFilePos;
-			this->TileSprite = obj.TileSprite;
-			return *this;
-		}
-	};
-	Tile tile;
 	Tile** Map;
-	sf::Vector2f MapSize;
-	sf::Vector2f TileStartPos;
-	sf::Vector2f TileCurrentPos;
-public :
-	GameMap(sf::Vector2f start, std::string TilefilePath, sf::Vector2f posInFile, sf::Window& window, sf::Vector2u tileSize);
-	void CreateCell(unsigned int xPos, unsigned int yPos, Tile& tile);
-	void SetRect(sf::Vector2f start, sf::Vector2f size);
-	void DrawMap(sf::RenderWindow& window);
+	int x_size;
+	int y_size;
+public:
+	//¬ данный момент здесь пусто. ћетоды будут добавлены, когда окончательно будет определены уровни, их размеры и используемые тайлы.
 };
 
 
