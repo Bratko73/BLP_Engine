0e620e66d2b832752a1cc6a871660f5d8a13147b
Xxone-ai <57640369+Xxone-ai@users.noreply.github.com>
2021-01-21 18:06:59 +0300

    минус глаза просто

---
+++ FileSystem.cpp
@@ -0,0 +1,140 @@
+#include "FileSystem.h"
+
+FileSystem::FileSystem(std::string path)
+{
+	this->path = path;
+}
+
+void FileSystem::changeFile(std::string newPath)
+{
+	this->path = newPath;
+}
+
+void FileSystem::loadLevel(std::vector <Gumba>& gumbas, std::vector <Turtle>& turtle, std::vector <BonusMushroom>& bonus, std::vector <Tile>& tiles, background& bg, std::map <char, Tile>& tileMap, GameMap& map)
+{
+	std::ifstream fin;
+	fin.open(path);
+	if (!fin.is_open())
+		return;
+	else
+	{
+		while (!fin.eof())
+		{
+			std::string tmp;
+			fin >> tmp;
+			if (tmp == "Gumba")
+			{
+				int count;
+				fin >> count;
+				std::string pathToSprite;
+				fin >> pathToSprite;
+				float speed;
+				fin >> speed;
+				int size;
+				fin >> size;
+				float gravity;
+				fin >> gravity;
+				int countOfFrames;
+				fin >> countOfFrames;
+				int firstFrameX;
+				fin >> firstFrameX;
+				int firstFrameY;
+				fin >> firstFrameY;  
+				int rangeBetweenFrames;
+				fin >> rangeBetweenFrames;
+				float animationSpeed;
+				fin >> animationSpeed;
+				int coordX;//Pizdec.txt
+				int coordY;
+				for (int i = 0; i < count; i++)
+				{
+					fin >> coordX;
+					fin >> coordY;
+					gumbas.push_back(Gumba(pathToSprite, speed, sf::FloatRect(coordX, coordY, size, size), gravity));
+					gumbas[i].setAnimationSettings(sf::Vector2i(size, size), sf::Vector2i(firstFrameX, firstFrameY), countOfFrames, rangeBetweenFrames, animationSpeed);
+				}
+			}
+			if (tmp == "Turtle")
+			{
+				int count;
+				fin >> count;
+				std::string pathToSprite;
+				fin >> pathToSprite;
+				float speed;
+				fin >> speed;
+				int sizeX;
+				fin >> sizeX;
+				int sizeY;
+				fin >> sizeY;
+				float gravity;
+				fin >> gravity;
+				float jump;
+				fin >> jump;
+				int countOfFrames;
+				fin >> countOfFrames;
+				int firstFrameX;
+				fin >> firstFrameX;
+				int firstFrameY;
+				fin >> firstFrameY;
+				int rangeBetweenFrames;
+				fin >> rangeBetweenFrames;
+				float animationSpeed;
+				fin >> animationSpeed;
+				int coordX;//Pizdec.txt
+				int coordY;
+				for (int i = 0; i < count; i++)
+				{
+					fin >> coordX;
+					fin >> coordY;
+					turtle.push_back(Turtle(pathToSprite, speed, sf::FloatRect(coordX, coordY, sizeX, sizeY), gravity, jump));
+					turtle[i].setAnimationSettings(sf::Vector2i(sizeX, sizeY), sf::Vector2i(firstFrameX, firstFrameY), countOfFrames, rangeBetweenFrames, animationSpeed);
+				}
+			}
+			if (tmp == "Bonus")
+			{
+				int count;
+				fin >> count;
+				std::string pathToSprite;
+				fin >> pathToSprite;
+				float speed;
+				fin >> speed;
+				int size;
+				fin >> size;
+				float gravity;
+				fin >> gravity;
+				int countOfFrames;
+				fin >> countOfFrames;
+				int firstFrameX;
+				fin >> firstFrameX;
+				int firstFrameY;
+				fin >> firstFrameY;
+				int rangeBetweenFrames;
+				fin >> rangeBetweenFrames;
+				float animationSpeed;
+				fin >> animationSpeed;
+				int coordX;//Pizdec.txt
+				int coordY;
+				for (int i = 0; i < count; i++)
+				{
+					fin >> coordX;
+					fin >> coordY;
+					bonus.push_back(BonusMushroom(pathToSprite, speed, sf::FloatRect(coordX, coordY, size, size), gravity));
+				    bonus[i].setAnimationSettings(sf::Vector2i(size, size), sf::Vector2i(firstFrameX, firstFrameY), countOfFrames, rangeBetweenFrames, animationSpeed);
+				}
+			}
+			if (tmp == "Bg")
+			{
+				
+			}
+			if (tmp == "tileMap")
+			{
+
+			}
+			if (tmp == "gameMap")
+			{
+
+			}
+
+		}
+	}
+}
